5d43c0fa565eebcfda3e6ab7dd534e7a14cedd12
Abhinav Jain <abhinav.jain0309@gmail.com>
2017-05-15 17:06:27 +0530

    Add files via upload

---
+++ AppCoreVideos/MetaCafeVideo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCoreVideos
+{
+    public class MetaCafeVideo
+    {
+        public string VideoTitle { get; set; }
+
+        public string DownloadUrl { get; set; }
+
+        public string VideoUrl { get; set; }
+    }
+}
