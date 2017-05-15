c2bb897d95b16cdb56f73d10fcdaa2fba4e0d3a7
Abhinav Jain <abhinav.jain0309@gmail.com>
2017-05-15 17:01:12 +0530

    Add files via upload

---
+++ AppCoreMusic/Mp3SkullMusic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCoreMusic
+{
+    public class Mp3SkullMusic
+    {
+        public string size { get; set; }
+        public string name { get; set; }
+        public string time { get; set; }
+        public string bitRate { get; set; }
+        public string downloadLink { get; set; }
+    }
+}
